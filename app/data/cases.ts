fc14a04f1d30a781d862718519d5c46e2f9d727b
Maikel Gudin <scalatelab@gmail.com>
2026-07-28 02:20:21 +0000

    Mejorar diseño Case

--- app/data/cases.ts
+++ app/data/cases.ts
@@ -2,18 +2,21 @@ export const cases = [
   {
     name: "Origenes",
     image: "/images/origenes.png",
+    href: "/casos/origenes",
     hasPage: true,
   },
 
   {
     name: "MyWave",
     image: "/images/mywave.png",
+    href: "/casos/mywave",
     hasPage: true,
   },
 
   {
     name: "Vicca",
     image: "/images/vicca.png",
+    href: "/casos/vicca",
     hasPage: true,
   },
 ];
