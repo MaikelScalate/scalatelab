221d552657faeb5935b291486fd7938c4c374365
MaikelScalate <scalatelab@gmail.com>
2026-08-04 19:46:09 +0100

    Mejorar diseño

--- app/components/layout/Navbar.tsx
+++ app/components/layout/Navbar.tsx
@@ -108,9 +108,14 @@ export default function Navbar() {
   href="#contacto"
   className="
     whitespace-nowrap
-    px-4
-    py-2
-    text-sm
+
+    px-3
+    py-1.5
+    text-[13px]
+
+    sm:px-4
+    sm:py-2
+    sm:text-sm
 
     lg:px-6
     lg:py-2
